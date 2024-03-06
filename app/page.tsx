85c8d8d442782cf1e7699fce0d9922fa1e2288be
Kingsley <kingsleybaidu99@gmail.com>
2024-03-06 17:21:07 +0000

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,154 @@
+'use client'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { InputText } from 'primereact/inputtext';
+import { apiData } from "@/lib/types";
 import Image from "next/image";
+import sunCloudImg from "@/public/sun-cloud.png";
+import cloudImg from "@/public/fluent--weather-cloudy-filled.svg";
+import rainyCloud from "@/public/rainy-cloud.svg";
+import cityImg from "@/public/cityImg.jpeg";
 
-export default function Home() {
+
+import 'primeicons/primeicons.css';
+        
+
+const Page = () => {
+  const [data, setData] = useState();
+  const [city, setCity] = useState<string>();
+
+  const apiKey = "634caabc9bb4672b24050648b3d9400d";
+  const cityInputtedByUser = "Nsawam";
+  let units = "imperial";
+  let temperatureSymobol = units === 'imperial' ? "°F" : "°C";
+  const icon = '';
+  let image = `https://openweathermap.org/img/wn/${icon}@4x.png`
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputtedByUser}&appid=${apiKey}&units=${units}`;
+
+  useEffect(() => {
+    axios.get(apiUrl)
+    .then((res) => setData(res.data))
+    .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(()=> console.log(data), [data])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <div className="flex flex-row">
+      <section className="flex flex-col w-[25%] h-screen border px-10 py-5 border-r-2 border-gray-100 shadow-xl bg-white">
+      <span className="p-input-icon-left">
+          <i className="pi pi-search pl-2" />
+          <InputText 
+          className="pl-7 border outline-none w-full h-10 rounded-md"
+          placeholder="search for city..." />
+      </span>
+      <div className="flex flex-col items-center border-gray-100 shadow-sm border-b-2 pb-8">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={sunCloudImg}
+          alt="My Image"
+          width={200}
+          height={200}
+          // layout="responsive"
+          // sizes="(min-width: 668px) 100vw, (min-width: 540px) 50vw, 320px"
         />
+        <div className="text-[70px]"> 12°C </div>
+        <div className="text-[#bababa]"> <span className="text-black"> Monday, </span> 16:00 </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="pt-[30px]">
+        <span className="flex flex-row justify-left items-center"> 
+        <Image
+          src={cloudImg}
+          alt=""
+          width={40}
+          height={40}
+          // layout="responsive"
+          // sizes="(min-width: 668px) 100vw, (min-width: 540px) 50vw, 320px"
+        />
+          <text>Mostly Cloudy</text>
+        </span>
+        <span className="flex flex-row justify-left items-center"> 
+        {/* #def4fc */}
+        <Image
+          src={rainyCloud}
+          alt=""
+          width={40}
+          height={40}
+          // layout="responsive"
+          // sizes="(min-width: 668px) 100vw, (min-width: 540px) 50vw, 320px"
+        />
+          <text>Rain - 30%</text>
+        </span>
       </div>
-    </main>
-  );
+      <span className="relative mt-5 flex justify-center items-center bg-black rounded-lg"> 
+        <Image
+          src={cityImg}
+          alt=""
+          width={300}
+          height={100}
+          className="h-[100px] rounded-lg opacity-80"
+        />
+        <span className="absolute text-white font-extrabold"> Accra </span>
+      </span>
+      </section>
+      <section className="w-[75%] px-[40px]">
+        <div id="navbar" className="flex flex-row justify-between items-center px-4 mt-8 h-12 border-gray-100 shadow-sm border-b-2">
+          <span className="flex flex-row justify-between w-[12%]">
+            <button> Today </button>
+            <button className="text-[#bababa]"> 5 days </button>
+          </span>
+          <span className="h-full flex flex-row items-center">
+            <span className="bg-[#000S] rounded-full bg-black text-white w-9 flex justify-center items-center h-9"> °C </span>
+            <span className="rounded-full w-9 flex justify-center items-center h-9"> °F </span>
+          </span>
+        </div>
+        <div>
+          <div> Description </div>
+          <div className="mt-[40px] flex flex-col pt-[50px]">
+            <span className="font-bold text-[20px] pl-6"> Highlights </span>
+            <div className=" grid grid-cols-3 grid-rows-2 gap-y-4">
+              <div className="rounded-[10px] shadow-md hover:shadow-xl flex flex-col justify-center items-center border w-[250px] py-8 h-[220px]">
+                <text className="text-[20px] text-[#bababa]"> Sunrise & Sunset </text>
+                <span>
+                  <text className="text-[30px]"> 6:30AM </text>
+                </span>
+                <span>
+                  <text className="text-[30px]"> 6:00PM </text>
+                </span>
+              </div>
+              <div className="rounded-[10px] shadow-md hover:shadow-xl flex flex-col justify-center items-center border w-[250px] py-8 h-[220px]">
+                <text className="text-[20px] text-[#bababa]"> Temperature </text>
+                <text className="font-semibold text-[50px]"> 12°C </text>
+                <text> Normal</text>
+              </div>
+              <div className="rounded-[10px] shadow-md hover:shadow-xl flex flex-col justify-center items-center border w-[250px] py-8 h-[220px]">
+                <text className="text-[20px] text-[#bababa]"> Min Temperature </text>
+                <text className="font-semibold text-[50px]"> 10°C </text>
+                <text> Normal</text>
+              </div>
+              <div className="rounded-[10px] shadow-md hover:shadow-xl flex flex-col justify-center items-center border w-[250px] py-8 h-[220px]">
+                <text className="text-[20px] text-[#bababa]"> Max Temperature </text>
+                <text className="font-semibold text-[50px]"> 15°C </text>
+                <text> Normal</text>
+              </div>
+              <div className="rounded-[10px] shadow-md hover:shadow-xl flex flex-col justify-center items-center border w-[250px] py-8 h-[220px]">
+                <text className="text-[20px] text-[#bababa]"> Humidity </text>
+                <text className="font-semibold text-[50px]"> 12% </text>
+                <text> Normal</text>
+              </div>
+              <div className="rounded-[10px] shadow-md hover:shadow-xl flex flex-col justify-center items-center border w-[250px] py-8 h-[220px]">
+                <text className="text-[20px] text-[#bababa]"> Wind Status </text>
+                <text className="font-semibold text-[50px]"> 7.70 <text className="text-[30px]"> km/h </text> </text>
+                <text> Normal</text>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
+
+
+
+export default Page;
